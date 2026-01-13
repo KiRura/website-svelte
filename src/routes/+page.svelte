753e3077a96018961bd39876c372956f607257db
KiRura <57588049+KiRura@users.noreply.github.com>
2026-01-14 04:58:04 +0900

    feat: くるくるヘルタを実装
    widthの異常とposition: fixedの機能不全が見られるため注意

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
-<script>
+<script lang="ts">
 	import { css, cx } from "styled-system/css";
-	import { container, separator } from "styled-system/recipes";
+	import { button, container } from "styled-system/recipes";
 	import ZZZ from "../component/ZZZ.svelte";
 	import { center } from "styled-system/patterns";
 	import { dev } from "$app/environment";
+	import herta from "$lib/assets/herta.webp";
+	import kurukuru from "$lib/assets/kurukuru.webp";
+	import { Presence } from "@ark-ui/svelte";
+
+	let isKurukuru = $state(false);
+	let kurukuruTimeout = $state<NodeJS.Timeout | null>(null);
 </script>
 
 <ZZZ
@@ -63,7 +69,6 @@
 			</h1>
 		</section>
 	</div>
-	<span class={separator()}></span>
 	<div class={cx(container(), css({ py: "16" }))}>
 		<img
 			src={`https://count.getloli.com/@KiRura-website-svelte${dev ? "-dev" : ""}?darkmode=auto`}
@@ -72,5 +77,66 @@
 			height="100px"
 			class={css({ mx: "auto" })}
 		/>
+	</div>
+	<div
+		class={css({
+			w: "fit",
+			mx: "auto",
+			filter: "drop-shadow(0 0 16px {colors.bg/80})",
+		})}
+	>
+		<button
+			class={cx(button({ variant: "plain" }), css({ h: "24" }))}
+			onclick={() => {
+				if (kurukuruTimeout) clearTimeout(kurukuruTimeout);
+				window.scrollTo({ top: 0, behavior: "smooth" });
+				isKurukuru = true;
+				kurukuruTimeout = setTimeout(() => {
+					isKurukuru = false;
+				}, 1000);
+			}}
+		>
+			<!-- data-state="open"の時だけwidthの計算に異常が発生する -->
+			<!-- position: fixedが機能しない -->
+			<!-- ChromeよりFirefoxの方が若干widthが細い差異が見られるためブラウザのバグと判断 -->
+			<Presence
+				present={isKurukuru}
+				class={css({
+					h: "24",
+					pos: "fixed",
+					bottom: "0",
+					zIndex: "docked",
+					_closed: {
+						animationName: "slide-to-bottom-full",
+						animationDuration: "slowest",
+						animationTimingFunction: "ease-in",
+					},
+				})}
+			>
+				<img
+					src={kurukuru}
+					alt="kurukuru~"
+					class={css({ h: "full", objectFit: "cover" })}
+				/>
+			</Presence>
+			<Presence
+				present={!isKurukuru}
+				class={css({
+					h: "full",
+					_open: {
+						animationName: "slide-from-bottom-full",
+						animationDuration: "slowest",
+						animationTimingFunction: "ease-in-smooth",
+					},
+				})}
+			>
+				<img
+					src={herta}
+					alt="herta"
+					loading="lazy"
+					class={css({ h: "full", objectFit: "cover" })}
+				/>
+			</Presence>
+		</button>
 	</div>
 </main>
