7dd745e6812236bcf309130bd22a9d6ba80c81ef
KiRura <57588049+KiRura@users.noreply.github.com>
2026-02-07 00:39:26 +0900

    feat: melt-uiを導入

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-	preprocess: vitePreprocess(),
-
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	kit: {
 		adapter: adapter(),
 		alias: {
@@ -12,5 +11,4 @@ const config = {
 		},
 	},
 };
-
 export default config;
