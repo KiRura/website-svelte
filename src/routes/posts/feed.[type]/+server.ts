090d2203fc7ae7957e832ef848550551210995b6
KiRura <57588049+KiRura@users.noreply.github.com>
2026-02-07 00:39:08 +0900

    feat: feedにatomを追加, imageを削除

--- src/routes/posts/feed.[type]/+server.ts
+++ src/routes/posts/feed.[type]/+server.ts
@@ -7,11 +7,14 @@ import { getPosts } from "$lib/cms";
 import type { PostWithContent } from "../../../interface/cms";
 
 export const GET: RequestHandler = async ({ url, params }) => {
-	const type = z.literal(["xml", "json"]).safeParse(params.type);
+	const type = z.literal(["xml", "json", "atom.xml"]).safeParse(params.type);
 	if (type.error)
-		return new Response(`Invalid type.\nShould be: feed.{xml, json}`, {
-			status: StatusCodes.BAD_REQUEST,
-		});
+		return new Response(
+			`Invalid type.\nShould be: feed.{xml, json, atom.xml}`,
+			{
+				status: StatusCodes.BAD_REQUEST,
+			},
+		);
 
 	const posts = await getPosts<PostWithContent>({
 		limit: 100,
@@ -35,13 +38,15 @@ export const GET: RequestHandler = async ({ url, params }) => {
 			lastUpdateDate = updatedAt;
 	}
 
+	const baseUrl = `${url.protocol}//${url.host}`;
+
 	const feed = new Feed({
 		title: "きるら",
 		description: "しがないSvelteサイト",
-		link: `${url.protocol}//${url.host}`,
+		link: baseUrl,
+		id: baseUrl,
 		language: "ja",
 		updated: lastUpdateDate,
-		image: `${url.protocol}//${url.host}/kirura/768p.png`,
 		author: {
 			name: "きるら",
 			email: "kirura@kirura.f5.si",
@@ -52,7 +57,7 @@ export const GET: RequestHandler = async ({ url, params }) => {
 	for (const post of posts.contents) {
 		feed.addItem({
 			title: post.title,
-			link: `${url.protocol}//${url.host}/posts/${post.id}`,
+			link: `${baseUrl}/posts/${post.id}`,
 			date: new Date(post.updatedAt),
 			id: post.id,
 			description: post.subtitle,
@@ -62,6 +67,15 @@ export const GET: RequestHandler = async ({ url, params }) => {
 		});
 	}
 
-	if (type.data === "json") return new Response(feed.json1());
-	return new Response(feed.rss2());
+	if (type.data === "json")
+		return new Response(feed.json1(), {
+			headers: { "content-type": "application/json" },
+		});
+	if (type.data === "atom.xml")
+		return new Response(feed.atom1(), {
+			headers: { "content-type": "application/xml" },
+		});
+	return new Response(feed.rss2(), {
+		headers: { "content-type": "application/xml" },
+	});
 };
